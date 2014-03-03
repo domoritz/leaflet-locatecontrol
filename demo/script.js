452c546f36443782cc70e47cd83448b1484e2e27
Dominik Moritz <domoritz@gmail.com>
2014-03-03 12:06:59 -0800

    Cloudmade disables free maps so let's switch to a free tile provider

--- demo/script.js
+++ demo/script.js
@@ -1,12 +1,11 @@
-var base_layer = new L.tileLayer(
-    'http://{s}.tile.cloudmade.com/63250e2ef1c24cc18761c70e76253f75/997/256/{z}/{x}/{y}.png',{
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-        maxZoom: 18
-    }
-);
+var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var osmAttrib='Map data © <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
+var osm = new L.TileLayer(osmUrl, {
+        attribution: osmAttrib
+});
 
 var map = new L.Map('map', {
-    layers: [base_layer],
+    layers: [osm],
     center: [51.505, -0.09],
     zoom: 10,
 	zoomControl: true
