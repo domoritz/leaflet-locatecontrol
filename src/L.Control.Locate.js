2ada0f636457d7d4840e1691e31a0416d93f75cc
Grady McCrackin <gmccrackin@neucadia.com>
2014-06-06 16:36:56 -0400

    Created an option to do a marker instead of a circleMarker.

--- src/L.Control.Locate.js
+++ src/L.Control.Locate.js
@@ -10,6 +10,7 @@ L.Control.Locate = L.Control.extend({
         drawCircle: true,
         follow: false,  // follow with zoom and pan the user's location
         stopFollowingOnDrag: false, // if follow is true, stop following when map is dragged (deprecated)
+        circleMarker: true,
         // range circle
         circleStyle: {
             color: '#136AEC',
@@ -222,16 +223,32 @@ L.Control.Locate = L.Control.extend({
             }
 
             var t = self.options.strings.popup;
-            if (!self._circleMarker) {
-                self._circleMarker = L.circleMarker(self._event.latlng, mStyle)
-                    .bindPopup(L.Util.template(t, {distance: distance, unit: unit}))
-                    .addTo(self._layer);
-            } else {
-                self._circleMarker.setLatLng(self._event.latlng)
-                    .bindPopup(L.Util.template(t, {distance: distance, unit: unit}))
-                    ._popup.setLatLng(self._event.latlng);
-                for (o in mStyle) {
-                    self._circleMarker.options[o] = mStyle[o];
+            if (self.options.circleMarker) {
+                if (!self._circleMarker) {
+                    self._circleMarker = L.circleMarker(self._event.latlng, mStyle)
+                        .bindPopup(L.Util.template(t, {distance: distance, unit: unit}))
+                        .addTo(self._layer);
+                } else {
+                    self._circleMarker.setLatLng(self._event.latlng)
+                        .bindPopup(L.Util.template(t, {distance: distance, unit: unit}))
+                        ._popup.setLatLng(self._event.latlng);
+                    for (o in mStyle) {
+                        self._circleMarker.options[o] = mStyle[o];
+                    }
+                }
+            }
+            else {
+                if (!self._marker) {
+                    self._marker = L.marker(self._event.latlng, mStyle)
+                        .bindPopup(L.Util.template(t, {distance: distance, unit: unit}))
+                        .addTo(self._layer);
+                } else {
+                    self._marker.setLatLng(self._event.latlng)
+                        .bindPopup(L.Util.template(t, {distance: distance, unit: unit}))
+                        ._popup.setLatLng(self._event.latlng);
+                    for (o in mStyle) {
+                        self._marker.options[o] = mStyle[o];
+                    }
                 }
             }
 
@@ -288,6 +305,7 @@ L.Control.Locate = L.Control.extend({
 
             self._layer.clearLayers();
             self._circleMarker = undefined;
+            self._marker = undefined;
             self._circle = undefined;
         };
 
