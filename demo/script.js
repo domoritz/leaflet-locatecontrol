cfcb383146894491722d18b77b8448effe8c5b69
Dominik Moritz <domoritz@gmail.com>
2015-01-26 17:36:16 -0800

    Use mapbox in demo because they have retina tiles

--- demo/script.js
+++ demo/script.js
@@ -1,11 +1,19 @@
 var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var osmAttrib='Map data © <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
 var osm = new L.TileLayer(osmUrl, {
-        attribution: osmAttrib
+    attribution: osmAttrib,
+    detectRetina: true
+});
+
+var token = 'pk.eyJ1IjoiZG9tb3JpdHoiLCJhIjoieENoTEhXUSJ9.kjCosRk1pmnOqTvfsjmgIg';
+var mapboxUrl = 'http://api.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}@2x.png?access_token=' + token;
+var mapboxAttrib = 'Map data © <a href="http://osm.org/copyright">OpenStreetMap</a> contributors. Tiles from <a href="https://www.mapbox.com">Mapbox</a>.';
+var mapbox = new L.TileLayer(mapboxUrl, {
+  attribution: mapboxAttrib
 });
 
 var map = new L.Map('map', {
-    layers: [osm],
+    layers: [mapbox],
     center: [51.505, -0.09],
     zoom: 10,
     zoomControl: true
