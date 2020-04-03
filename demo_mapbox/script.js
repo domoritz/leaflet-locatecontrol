4c5eab8696fc1386ef0e366e78b643e67cc679cc
Dominik Moritz <domoritz@gmail.com>
2020-04-03 10:20:53 -0700

    Cycle mapbox token

--- demo_mapbox/script.js
+++ demo_mapbox/script.js
@@ -1,5 +1,5 @@
 // please replace this with your own mapbox token!
-L.mapbox.accessToken = 'pk.eyJ1IjoiZG9tb3JpdHoiLCJhIjoiY2o0OHZuY3MwMGo1cTMybGM4MTFrM2dxbCJ9.yCQe43DMRqobazKewlhi9w';
+L.mapbox.accessToken = 'pk.eyJ1IjoiZG9tb3JpdHoiLCJhIjoiY2s4a2dlY3MwMDFtbzNtbXd1cWs2OWx3ZSJ9._Q0sobia1rqR6OWXhzdUOA';
 
 var map = L.mapbox.map('map').setView([51.505, -0.09], 10);
 L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v10').addTo(map);
