9b321aefe0301261304501574a81a14056f40ca6
Dominik Moritz <domoritz@gmail.com>
2020-04-03 10:32:58 -0700

    Use restricted token

--- demo/script.js
+++ demo/script.js
@@ -6,7 +6,7 @@ var osm = new L.TileLayer(osmUrl, {
 });
 
 // please replace this with your own mapbox token!
-var token = 'pk.eyJ1IjoiZG9tb3JpdHoiLCJhIjoiY2s4a2dlY3MwMDFtbzNtbXd1cWs2OWx3ZSJ9._Q0sobia1rqR6OWXhzdUOA';
+var token = 'pk.eyJ1IjoiZG9tb3JpdHoiLCJhIjoiY2s4a2d0OHp3MDFxMTNmcWoxdDVmdHF4MiJ9.y9-0BZCXJBpNBzEHxhFq1Q';
 var mapboxUrl = 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/{z}/{x}/{y}@2x?access_token=' + token;
 var mapboxAttrib = 'Map data © <a href="http://osm.org/copyright">OpenStreetMap</a> contributors. Tiles from <a href="https://www.mapbox.com">Mapbox</a>.';
 var mapbox = new L.TileLayer(mapboxUrl, {
--- demo_mapbox/script.js
+++ demo_mapbox/script.js
@@ -1,5 +1,5 @@
 // please replace this with your own mapbox token!
-L.mapbox.accessToken = 'pk.eyJ1IjoiZG9tb3JpdHoiLCJhIjoiY2s4a2dlY3MwMDFtbzNtbXd1cWs2OWx3ZSJ9._Q0sobia1rqR6OWXhzdUOA';
+L.mapbox.accessToken = 'pk.eyJ1IjoiZG9tb3JpdHoiLCJhIjoiY2s4a2d0OHp3MDFxMTNmcWoxdDVmdHF4MiJ9.y9-0BZCXJBpNBzEHxhFq1Q';
 
 var map = L.mapbox.map('map').setView([51.505, -0.09], 10);
 L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v10').addTo(map);
