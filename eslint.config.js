1e8848facbf2fba0efd8fae8436893e5e613a196
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2025-12-29 01:16:25 +0100

    chore: replace `stylelint` with `@eslint/css` for linting CSS

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 import { defineConfig, globalIgnores } from "eslint/config";
+import css from "@eslint/css";
 import globals from "globals";
 
 export default defineConfig([
   globalIgnores(["dist/*"]),
+  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"], rules: { "css/use-baseline": "off", "css/no-important": "off" } },
   {
     files: ["**/*.js"],
     languageOptions: {
