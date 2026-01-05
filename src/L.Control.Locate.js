81f04eae4773edd40233b492b38a308c1185c637
Yohan Boniface <yohanboniface@free.fr>
2026-01-05 16:53:17 +0100

    feat: stop locate when control is removed from map
    If there is an active location when the control is removed from
    the map, let's call stop to deactivate it, and remove event
    listeners (zoomEnd…).

--- src/L.Control.Locate.js
+++ src/L.Control.Locate.js
@@ -380,6 +380,13 @@ const LocateControl = Control.extend({
   },
 
   /**
+   * Called when control is removed from the map.
+   */
+  onRemove() {
+    this.stop()
+  },
+
+  /**
    * This method is called when the user clicks on the control.
    */
   _onClick() {
